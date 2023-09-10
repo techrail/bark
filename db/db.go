47720617404799ea8b94bc204fcb78c7aef658dd
deepto <ddepp98@outlook.com>
2023-09-11 05:11:10 +0530

    added db connection

---
+++ db/db.go
@@ -0,0 +1,37 @@
+package db
+
+import (
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq" //needed for postgres driver
+)
+
+type Database struct {
+	Client *sqlx.DB
+}
+
+func NewDatabase() (*Database, error) {
+	connectionString := fmt.Sprintf(
+		"host=%s port=%s user=%s dbname=%s password=%s sslmode=%s",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USERNAME"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_SSL_MODE"),
+	)
+
+	dbConn, err := sqlx.Connect("postgres", connectionString)
+
+	if err != nil {
+		return &Database{}, fmt.Errorf("error connecting to: %w", err)
+	}
+	return &Database{Client: dbConn}, nil
+}
+
+func (d *Database) Ping(ctx context.Context) error {
+	return d.Client.DB.PingContext(ctx)
+}

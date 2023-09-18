3d321e53a15cd9f7b9fecbc74853c80feb8f46b0
Harikrishna-28 <harikrishnac2002@gmail.com>
2023-09-18 13:42:30 +0530

    add env for connection string

--- resources/db.go
+++ resources/db.go
@@ -1,11 +1,12 @@
 package resources
 
 import (
-	`context`
-	`fmt`
+	"context"
+	"fmt"
+	"os"
 
-	`github.com/jmoiron/sqlx`
-	_ `github.com/lib/pq`
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 )
 
 // BarkPostgresDb wraps the sqlx.DB in a custom struct to use it as a receiver for query functions
@@ -33,7 +34,12 @@ func InitDatabase() error {
 }
 
 func OpenDatabase() (*BarkPostgresDb, error) {
-	dbConn, err := sqlx.Open("postgres", "postgres://vaibhavkaushal:vaibhavkaushal@127.0.0.1:5432/bark?sslmode=disable")
+	databaseConnectionString := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=%s",
+		os.Getenv("DB_USERNAME"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"), os.Getenv("DB_SSL_MODE"),
+	)
+
+	dbConn, err := sqlx.Open("postgres", databaseConnectionString)
 
 	if err != nil {
 		return &BarkPostgresDb{}, fmt.Errorf("E#1KDW57 - error connecting to db: %w", err)

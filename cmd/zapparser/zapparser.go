20884bb00dc0235e8e4d0032c169fa5257923834
Hrishikesh Patil <hrishikesh.patil@yandex.com>
2023-10-18 22:23:16 +0530

    zapparser

--- cmd/zapparser/zapparser.go
+++ cmd/zapparser/zapparser.go
@@ -9,7 +9,9 @@ import (
 	"github.com/techrail/bark/client"
 	"github.com/techrail/bark/constants"
 	"github.com/techrail/bark/typs/jsonObject"
+	"math"
 	"os"
+	"strconv"
 	"time"
 )
 
@@ -17,12 +19,21 @@ import (
 // There is no guarantee that the naming we're using here will be followed.
 // The naming is inspired from zap.NewProduction() Logger's output.
 type ZapLog struct {
-	Level      string `json:"level"`
-	Timestamp  string `json:"timestamp"`
-	Caller     string `json:"caller"` // Not used right now.
-	Message    string `json:"msg"`
-	PID        int    `json:"pid"`        // Not used right now.
-	Stacktrace string `json:"stacktrace"` // Not used right now.
+	Level         string  `json:"level"`
+	UnixTimestamp float64 `json:"timestamp"`
+	TextTimestamp string  `json:"timestamp"`
+	Caller        string  `json:"caller"` // Not used right now.
+	Message       string  `json:"msg"`
+	PID           int     `json:"pid"`        // Not used right now.
+	Stacktrace    string  `json:"stacktrace"` // Not used right now.
+}
+
+// isUnixTimestamp checks if a string is unix timestamp or not
+func isUnixTimestamp(str string) bool {
+	if _, err := strconv.ParseInt(str, 10, 64); err != nil {
+		return false
+	}
+	return true
 }
 
 func main() {
@@ -31,7 +42,7 @@ func main() {
 	url := flag.String("server", "http://localhost:8080/", "Bark server url")
 	service := flag.String("service", "No service name", "Service Name")
 	session := flag.String("session", "No session name", "Session Name")
-	layout := flag.String("layout", "2006-01-02T15:04:05.999-0700", "Time format")
+	format := flag.String("format", time.RFC3339, "Time format")
 	file, err := os.ReadFile(*filePath)
 	if err != nil {
 		fmt.Println("E#1M2OQ5 - No log file found!!")
@@ -43,17 +54,27 @@ func main() {
 		logToBeSent := ZapLog{}
 		err := json.Unmarshal(scanner.Bytes(), &logToBeSent)
 		if err != nil {
-			err = fmt.Errorf("E#1M1AZO - Error while sending a log. E: %s\n", err.Error())
+			fmt.Printf("E#1M1AZO - Error while sending a log. E: %s\n", err.Error())
 		}
-		timestamp, err := time.Parse(*layout, logToBeSent.Timestamp)
 		logToSend := client.RawLog{
 			Message:     logToBeSent.Message,
-			LogTime:     timestamp,
 			LogLevel:    logToBeSent.Level,
 			SessionName: *session,
 			ServiceName: *service,
 			MoreData:    jsonObject.Typ{},
 		}
+
+		var timestamp time.Time
+
+		if logToBeSent.UnixTimestamp == 0 {
+			timestamp, _ = time.Parse(*format, logToBeSent.TextTimestamp)
+		} else {
+			sec, dec := math.Modf(logToBeSent.UnixTimestamp)
+			timestamp = time.Unix(int64(sec), int64(dec*(1e9)))
+		}
+
+		logToSend.LogTime = timestamp
+
 		err = logger.Raw(logToSend, true)
 		if err != nil {
 			fmt.Printf("E#1M1AYF - Error while sending the log. E: %s\n", err.Error())

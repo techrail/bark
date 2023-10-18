98b1edb45f9c99f9e9a201f3a1e2a720db35664a
Hrishikesh Patil <hrishikesh.patil@yandex.com>
2023-10-18 23:28:03 +0530

    update zap parser, and added documentation for it

--- cmd/zapparser/readme.md
+++ cmd/zapparser/readme.md
@@ -1,17 +1,82 @@
-## Zap Parser
-Bark's zap parser is a utility that allows you to save logs generated with uber's zap library into bark server.
+# Zap Parser for Bark
 
-### How to use it?
-You can use the suitable executable zapparser file, and pass any/all the following parameters:
+🚀 **Zap Parser** is a utility that simplifies the process of transferring logs generated using Uber's Zap library to a [Bark](https://github.com/techrail/bark) server for easy monitoring and analysis.
 
-- `file` - the path of the zap's log file, this defaults to `log.txt` if not specified.
-- `server` - the url of the bark server, this defaults to `http://localhost:8080/` if not specified.
-- `service` - any service name that you want to specify, this defaults to `No service name` if not specified.
-- `session` - any session name that you want to specify, this defaults to `No session name` if not specified.
+## Features
 
-This is how you would execute the command,
+- Import logs from Uber's Zap library into Bark.
+- Customizable options for specifying log file path, service name, and session name.
+- Hassle-free integration with Bark for log analysis.
+
+## Table of Contents
+
+- [Getting Started](#getting-started)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Getting Started
+
+### Prerequisites
+
+Before using the Zap Parser, you need to have the following:
+
+- An executable suitable for your platform (e.g., `zapparser.exe` for Windows).
+- A Bark database or server URL for sending log data.
+
+### Manual Build
+
+1. Clone this repository to your local machine:
+
+   ```bash
+   git clone https://github.com/techrail/bark
+   ```
+
+2. Navigate to the project directory:
+
+   ```bash
+   cd zapparser
+   ```
+
+3. Execute the Zap Parser with the desired parameters, as explained in the [Usage](#usage) section.
+
+## Usage
+
+You can execute the Zap Parser by running the following command, customizing the options to your needs:
 
 ```shell
-./zapparser.exe -file="log.txt" -server="http://localhost:8080/" -service="example service" -session="example session"
+./zapparser.exe -file="log.txt" -db="postgres://auth_user:pass@localhost:5432/bark?sslmode=disable" -service="example service" -session="example session"
 ```
 
+- `-file` (optional): The path to the Zap log file you want to process. Defaults to `log.txt`.
+- `-db` (or `-server`) (required): The URL of the Bark server or the database connection URL.
+- `-service` (optional): The name of the service associated with the log entries. Defaults to "No service name."
+- `-session` (optional): The name of the session for the log entries. Defaults to "No session name."
+- `-format` (optional): Specifies a custom time format for timestamps in log entries (e.g., "2006-01-02 15:04:05").
+
+By executing this command, the Zap Parser will process the specified Zap log file and send the log entries to the Bark server.
+
+### Timestamp Handling
+
+The Zap Parser is designed to handle timestamps in log entries gracefully. If the input log file contains a Unix timestamp in the log entries, it will be used for timestamp processing. However, if log entries do not include Unix timestamps, the parser will default to using the RFC3339 format for timestamps.
+
+You can further customize the timestamp format using the `-format` option. This allows you to specify a time format other than RFC3339, giving you flexibility in how timestamps are interpreted. The `-format` option accepts standard Go time layout formats.
+
+By providing this flexibility, the Zap Parser ensures that log entries with different timestamp formats can be effectively processed and sent to the Bark server.
+
+> [!WARNING]  
+> Critical content demanding immediate user attention due to potential risks.
+
+## Contributing
+
+If you'd like to contribute to this project, please read our [Contribution Guidelines](../../CONTRIBUTING.md).
+
+## License
+
+This project is licensed under the [MIT License](../../LICENSE).
+
+---
+
+Happy logging with Bark! 🌟
+
+**Zap Parser for Bark** is not affiliated with or endorsed by Uber Technologies, Inc.
--- cmd/zapparser/zapparser.go
+++ cmd/zapparser/zapparser.go
@@ -11,50 +11,78 @@ import (
 	"github.com/techrail/bark/typs/jsonObject"
 	"math"
 	"os"
-	"strconv"
 	"time"
 )
 
-// ZapLog represents a single log in uber zap log file.
-// There is no guarantee that the naming we're using here will be followed.
-// The naming is inspired from zap.NewProduction() Logger's output.
+// ZapLog represents a single log entry in an Uber Zap log file.
 type ZapLog struct {
-	Level         string  `json:"level"`
-	UnixTimestamp float64 `json:"timestamp"`
-	TextTimestamp string  `json:"timestamp"`
-	Caller        string  `json:"caller"` // Not used right now.
-	Message       string  `json:"msg"`
-	PID           int     `json:"pid"`        // Not used right now.
-	Stacktrace    string  `json:"stacktrace"` // Not used right now.
-}
-
-// isUnixTimestamp checks if a string is unix timestamp or not
-func isUnixTimestamp(str string) bool {
-	if _, err := strconv.ParseInt(str, 10, 64); err != nil {
-		return false
-	}
-	return true
+	Level         string  `json:"level"`      // Log level, e.g., "info", "error", etc.
+	UnixTimestamp float64 `json:"timestamp"`  // Unix timestamp.
+	TextTimestamp string  `json:"timestamp"`  // Text-based timestamp (alternative to Unix timestamp).
+	Caller        string  `json:"caller"`     // Not used at the moment.
+	Message       string  `json:"msg"`        // Log message.
+	PID           int     `json:"pid"`        // Not used at the moment.
+	Stacktrace    string  `json:"stacktrace"` // Not used at the moment.
 }
 
 func main() {
 	var numberOfLogs = 0
+
+	// Define command-line flags and set default values.
 	filePath := flag.String("file", "log.txt", "File path to read logs from")
-	url := flag.String("server", "http://localhost:8080/", "Bark server url")
+	server := flag.String("server", "", "Bark server URL")
+	db := flag.String("db", "", "Bark database connection URL")
 	service := flag.String("service", "No service name", "Service Name")
 	session := flag.String("session", "No session name", "Session Name")
 	format := flag.String("format", time.RFC3339, "Time format")
+	help := flag.Bool("help", false, "Show help")
+
+	// Parse command-line arguments.
+	flag.Parse()
+
+	// Read the log file specified by the -file flag.
 	file, err := os.ReadFile(*filePath)
 	if err != nil {
-		fmt.Println("E#1M2OQ5 - No log file found!!")
+		fmt.Println("E#1M2OQ5 - Error reading file: No log file found!!")
+		*help = true
 	}
+
+	// Check if either -server or -db is provided, or display the help message.
+	if *server == "" && *db == "" {
+		fmt.Println("E#1M4Y1P - Error connecting to the server: Need either -server or -db connection URL.")
+		*help = true
+	}
+
+	// Display help instructions if -help is provided or any invalid input.
+	if *help {
+		fmt.Println("Usage: zap-parser [options]")
+		fmt.Println("Options:")
+		fmt.Println("  -file <filepath>   : Specify the path to the log file (default: log.txt).")
+		fmt.Println("  -server <url>     : Set the Bark server URL (required if -db is not provided).")
+		fmt.Println("  -db <url>         : Set the Bark database connection URL (required if -server is not provided).")
+		fmt.Println("  -service <name>   : Specify the service name (default: No service name).")
+		fmt.Println("  -session <name>   : Specify the session name (default: No session name).")
+		fmt.Println("  -format <timefmt> : Set the time format (default: RFC3339).")
+		fmt.Println("  -help             : Show this help message.")
+		return
+	}
+
+	// Initialize the Bark client based on the provided -server or -db flag.
+	var logger *client.Config
+	if *server != "" {
+		logger = client.NewClient(*server, constants.Info, *service, *session, false, true)
+	} else {
+		logger = client.NewClientWithServer(*db, constants.Info, *service, *session, false)
+	}
+
+	// Process and insert logs from the specified log file.
 	scanner := bufio.NewScanner(bytes.NewReader(file))
 	scanner.Split(bufio.ScanLines)
-	logger := client.NewClient(*url, constants.Info, *service, *session, false, true)
 	for scanner.Scan() {
 		logToBeSent := ZapLog{}
 		err := json.Unmarshal(scanner.Bytes(), &logToBeSent)
 		if err != nil {
-			fmt.Printf("E#1M1AZO - Error while sending a log. E: %s\n", err.Error())
+			fmt.Printf("E#1M1AZO - Error while sending a log. Error: %s\n", err.Error())
 		}
 		logToSend := client.RawLog{
 			Message:     logToBeSent.Message,
@@ -77,10 +105,13 @@ func main() {
 
 		err = logger.Raw(logToSend, true)
 		if err != nil {
-			fmt.Printf("E#1M1AYF - Error while sending the log. E: %s\n", err.Error())
+			fmt.Printf("E#1M1AYF - Error while sending the log. Error: %s\n", err.Error())
 		}
 		numberOfLogs++
 	}
+
+	// Wait for pending logs to be sent.
 	logger.WaitAndEnd()
-	fmt.Printf("Inserted %d logs into bark.\n", numberOfLogs)
+
+	fmt.Printf("Inserted %d logs into Bark.\n", numberOfLogs)
 }

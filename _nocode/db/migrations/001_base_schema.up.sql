b3d8c9193b187d12a92188d8721639dd4a8235b6
Vansh Bhagat <79078400+bhagatvansh@users.noreply.github.com>
2023-10-26 23:51:09 +0530

    Changed field name from session_name to service_instance_name

--- _nocode/db/migrations/001_base_schema.up.sql
+++ _nocode/db/migrations/001_base_schema.up.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS app_log
     log_time     TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT (NOW() AT TIME ZONE ('utc')),
     log_level    VARCHAR(16)                 NOT NULL DEFAULT 'info',
     service_name VARCHAR(64)                 NOT NULL DEFAULT 'def_svc',
-    session_name VARCHAR(64)                 NOT NULL DEFAULT 'def_svc_instance',
+    sevice_instance_name VARCHAR(64)         NOT NULL DEFAULT 'def_svc_instance',
     code         VARCHAR(16)                 NOT NULL DEFAULT '000000',
     msg          TEXT                        NOT NULL DEFAULT '_no_msg_supplied_',
     more_data    jsonb                       NOT NULL DEFAULT '{}'::jsonb
@@ -16,7 +16,7 @@ COMMENT ON COLUMN app_log.id IS 'Unique log ID, Primary Key, Auto-incrementing n
 COMMENT ON COLUMN app_log.log_time IS 'UTC time when the log occurred. Defaults to "now()"';
 COMMENT ON COLUMN app_log.log_level IS 'Severity level - INFO, WARNING, ERROR, PANIC etc.';
 COMMENT ON COLUMN app_log.service_name IS 'Name of the service that sent this log (defaults to "def_svc")';
-COMMENT ON COLUMN app_log.session_name IS 'Name of the service instance (or Pod Name) that sent this log (defaults to "def_sess")';
+COMMENT ON COLUMN app_log.service_instance_name IS 'Name of the service instance (or Pod Name) that sent this log (defaults to "def_svc_instance")';
 COMMENT ON COLUMN app_log.code IS 'Unique Code of the log (LMID) - to be sent by the caller (defaults to "000000")';
 COMMENT ON COLUMN app_log.msg IS 'Actual log message as text';
 COMMENT ON COLUMN app_log.more_data IS 'Anything else that needs to be saved alongside the log entry';
